8537ffc5a7c4dd7f78c186f9d7e1fd23fc5d65f6
郎鴻楓 <57393934@qq.com>
2019-08-20 15:25:32 +0800

    submit

---
+++ LHTestDemo/LHTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHTestDemo
+//
+//  Created by 郎鴻楓 on 2019/8/20.
+//  Copyright © 2019 YiWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHTestDemo/LHTestDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LHTestDemo
+//
+//  Created by 郎鴻楓 on 2019/8/20.
+//  Copyright © 2019 YiWang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
